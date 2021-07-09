38a1d68d934de4824f49253670dbacf0f7265a8c
Cristian Buc Calderon <33019683+CristianBucCalderon@users.noreply.github.com>
2021-07-09 16:50:50 -0400

    Update oscar_test.py

--- oscar_test.py
+++ oscar_test.py
@@ -2,6 +2,18 @@ import numpy
 import argparse
 import pickle
 
+CLI = argparse.ArgumentParser()
+CLI.add_argument("--machine", type = str, default = 'x7')
+CLI.add_argument("--method", type = str, default = 'ddm')
+CLI.add_argument("--nburnin", type = int, default = 0)
+
+args = CLI.parse_args()
+print(args)
+
+x =args.parameter1 
+y =args.parameter2
+z =args.parameter3
+
 def equation(x1,x2,x3):
   return x1 + x2 + x3
 
